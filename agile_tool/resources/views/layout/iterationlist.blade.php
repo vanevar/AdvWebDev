c639e7e33463ccb70038775149df76daf396b43e
rohit19j91 <rohit19j91@gmail.com>
2018-01-28 19:33:05 +0100

    * Updated the views in the project
    * Updated the views in the project with S No.

--- agile_tool/resources/views/layout/iterationlist.blade.php
+++ agile_tool/resources/views/layout/iterationlist.blade.php
@@ -6,10 +6,19 @@
 	<div class="input-group-addon"><a href="http://google.com"> <i class="fa fa-plus" aria-hidden="true"></i></a></div>
 	 </div>
       <p class="lead">All the Iterations related to the project.</p>
+ <ul>
+  <div class="row">
+        <div class="col-1"><strong>S No.</strong></div>
+        <div class="col-2"><strong>Deadline</strong></div>
+        <div class="col-2"><strong>Project Name</strong></div>
+        <div class="col-1"><strong>Options</strong></div>
+   </div>
+  </ul>
 
-@foreach ($iteration as $eiteration)
+@foreach ($iteration as $index => $eiteration)
       <ul>
   <div class="row">
+        <div class="col-1">{{$index+1}}</div>
         <div class="col-2">{{$eiteration->deadline}}</div>
         <div class="col-2">{{$eiteration->project_id}}</div>
         <div class="col-1">
--- agile_tool/resources/views/layout/tasklist.blade.php
+++ agile_tool/resources/views/layout/tasklist.blade.php
@@ -6,10 +6,21 @@
 	<div class="input-group-addon"><a href="http://google.com"> <i class="fa fa-plus" aria-hidden="true"></i></a></div>
 	 </div>
       <p class="lead">All the Tasks related to the project.</p>
+ <ul>
+  <div class="row">
+        <div class="col-1"><strong>S No.</strong></div>
+        <div class="col-2"><strong>Title</strong></div>
+        <div class="col-2"><strong>Description</strong></div>
+        <div class="col-2"><strong>Status</strong></div>
+        <div class="col-1"><strong>Owner</strong></div>
+        <div class="col-1"><strong>Options</strong></div>
+   </div>
+  </ul>
 
-@foreach ($task as $etask)
+@foreach ($task as $index => $etask)
       <ul>
   <div class="row">
+        <div class="col-1">{{$index+1}}</div>
         <div class="col-2">{{$etask->title}}</div>
         <div class="col-2">{{$etask->description}}</div>
         <div class="col-2">{{$etask->status_id}}</div>
