cb68462278ed748f545885f3f8658d4472b510bb
rohit19j91 <rohit19j91@gmail.com>
2018-01-29 18:38:24 +0100

    * edited the create project section
    Removed the dropdown to select the project role.

--- agile_tool/resources/views/layout/createprojectlayout.blade.php
+++ agile_tool/resources/views/layout/createprojectlayout.blade.php
@@ -14,18 +14,6 @@
     <input class="form-control" type="text" id="projectdescription">
   </div>
 </div>
- <div class="form-group row">
-    <label for="projectroler" class="col-2 col-form-label">Project Role</label>
-    <div class="col-10">
-    <select class="form-control" id="projectrole">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
-  </div>
-  </div>
   <div class="form-group row">
   <div class="col-8" align="center">
     <br><br><button type="submit" class="btn btn-primary">Submit</button>
