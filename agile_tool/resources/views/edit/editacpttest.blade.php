be7305e59e9960875ca1f203894713a2f8a3b6fd
rohit19j91 <rohit19j91@gmail.com>
2018-01-30 23:12:16 +0100

    * Updated all the edit pages with appropriate code
    Added the URLs and customized variables for it !

--- agile_tool/resources/views/edit/editacpttest.blade.php
+++ agile_tool/resources/views/edit/editacpttest.blade.php
@@ -20,25 +20,15 @@
 <div class="form-group row">
   <label for="acpttestdesc" class="col-2 col-form-label">Description</label>
   <div class="col-10">
-    <textarea class="form-control" rows="3" id="acpttestdesc"></textarea>
+    <textarea class="form-control" rows="3" id="acpttestdesc">{{ $acceptance_test->description }}</textarea>
   </div>
 </div>
 <div class="form-group row">
   <label for="acpttestresults" class="col-2 col-form-label">Test Results</label>
   <div class="col-10">
-    <textarea class="form-control" rows="3" id="acpttestresults"></textarea>
+    <textarea class="form-control" rows="3" id="acpttestresults">{{ $acceptance_test->test_result }}</textarea>
   </div>
 </div>
- <div class="form-group row">
-    <label for="acpttestfeature" class="col-2 col-form-label">Feature</label>
-    <div class="col-10">
-    <select class="form-control" id="acpttestfeature">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-    </select>
-  </div>
-  </div>
   <div class="form-group row">
     <label for="acpttestbug" class="col-2 col-form-label">Bug Id</label>
     <div class="col-10">
