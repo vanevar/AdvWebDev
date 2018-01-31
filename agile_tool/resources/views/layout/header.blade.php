e1f1f43c702fc33a6267f6925302c467f1fa6bb7
rohit19j91 <rohit19j91@gmail.com>
2018-01-31 03:27:05 +0100

    * Added the headers to all the files and Updated the login page
    Updated the login page with new fonts and formats

--- agile_tool/resources/views/layout/header.blade.php
+++ agile_tool/resources/views/layout/header.blade.php
@@ -2,7 +2,50 @@
       <!-- Fixed navbar -->
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <a class="navbar-brand" href="/">Advanced Web Development</a>
-          </form>
-        </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
+                <!--
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                -->
+    
+                <!-- Authentication Links -->
+                @guest
+                <li class="nav-item active" >
+                    <a class="nav-link" href="{{ route('login') }}">Login <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active" >
+                    <a class="nav-link" href="{{ route('register') }}">Register <span class="sr-only">(current)</span></a>
+                </li>
+                @else
+                <li class="nav-item">
+                    <a class="nav-link" href="#" >
+                        Welcome, {{ Auth::user()->firstname }} 
+                    </a>
+                </li>
+                <li class="nav-item active">
+                        <a class="nav-link" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                </li>
+                @endguest
+    
+            </div>
       </nav>
     </header>
